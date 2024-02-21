318efcd897cf9f30ed60cc4973f016187239eae5
drogojinaru <dumitru.rogojinaru@icloud.com>
2024-02-21 18:30:46 +0200

    woohoo push on main

---
+++ src/main/kotlin/com/example/Exercise1.kt
@@ -0,0 +1,30 @@
+package com.example
+
+class Exercise1 {
+
+  fun exercise1() {
+
+    val hello1 = "Hello"
+    val hello2 = "Hello"
+
+
+    println("are those ref eq? : , ${ hello1 === hello2 }")
+    println("are those struct eq? : , ${ hello1 == hello2 }")
+
+    var int = 2988
+
+    val any: Any = "The Any type is the root of Kotlin class hierarchy"
+    if (any is String) {
+      println(any.uppercase())
+    }
+
+    val theMostIndentedStringEverWritten = """    1
+        |   11
+        |  111
+        | 1111
+    """.trimMargin()
+
+    println(theMostIndentedStringEverWritten)
+  }
+
+}
---
+++ src/main/kotlin/com/example/Exercise2.kt
@@ -0,0 +1,28 @@
+package com.example
+
+class Exercise2 {
+
+  fun exercise2() {
+
+    val float1: Float? = -3847.384f
+    val float2: Float? = (-3847.384).toFloat()
+
+    val shortArray = shortArrayOf(1,2,3,4,5)
+
+    val shortArray100 = Array<Int?>(40) { i -> (i + 1) * 5 }
+
+    val charArray = charArrayOf('a', 'b', 'c')
+
+    val x: String? = "I AM AN UPPERCASE"
+
+    val z = x?.lowercase() ?: "I give up"
+    print(z)
+
+
+    val y = x?.lowercase()?.replace("am", "am not")
+    print(y)
+
+    val myNotNullVal: Int? = 4
+    myNotNullVal!!.toDouble()
+  }
+}
